6fcac864fddfadb4a38b103cee27e74597a0b728
baselsharaf <waelbk999@gmail>
2026-04-21 15:15:41 +0300

    add best seller list view item part 2

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff100B20);
+
+const kGtSectraFine = 'GT Sectra Fine';
+
+const kTranstionDuration = Duration(milliseconds: 250);
