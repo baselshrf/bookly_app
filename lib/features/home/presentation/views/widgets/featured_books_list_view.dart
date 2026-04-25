ba6f9f245d3f6cc8ab66fc64a5b325ea328a7a77
baselsharaf <waelbk999@gmail>
2026-04-25 16:40:40 +0300

    display featured books

--- lib/features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,6 +1,10 @@
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured%20cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
@@ -8,29 +12,45 @@ class FeaturedBooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: GestureDetector(
-              onTap: () {
-                GoRouter.of(
-                  context,
-                ).push(AppRouter.kBookDetailsView, extra: state.books[index]);
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              itemCount: state.books.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: GestureDetector(
+                    onTap: () {
+                      GoRouter.of(context).push(
+                        AppRouter.kBookDetailsView,
+                        extra: state.books[index],
+                      );
+                    },
+                    child: FeaturedListViewItem(
+                      imageUrl:
+                          state
+                              .books[index]
+                              .volumeInfo!
+                              .imageLinks
+                              ?.thumbnail ??
+                          '',
+                    ),
+                  ),
+                );
               },
-              child: FeaturedListViewItem(
-                imageUrl:
-                    state.books[index].volumeInfo.imageLinks?.thumbnail ?? '',
-              ),
             ),
           );
-        },
-      ),
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
