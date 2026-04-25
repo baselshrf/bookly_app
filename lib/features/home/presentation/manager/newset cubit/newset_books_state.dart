dc36ff49dbb117259e00d3c99fd28dcefbaaf290
baselsharaf <waelbk999@gmail>
2026-04-25 14:50:15 +0300

    create featured cubit

---
+++ lib/features/home/presentation/manager/newset cubit/newset_books_state.dart
@@ -0,0 +1,10 @@
+part of 'newset_books_cubit.dart';
+
+sealed class NewsetBooksState extends Equatable {
+  const NewsetBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class NewsetBooksInitial extends NewsetBooksState {}
